bb36ab3765dab6c7c9ff17fdb46977b0eccdc1eb
Mateus Morselli <matmorselli@gmail.com>
2025-03-24 03:53:07 -0300

    implementação de um teste de 2 comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,29 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('Teste de adição de 2 comentários', () => {
+        render(<PostComment/>);
+
+        fireEvent.change(screen.getByTestId('elemento-textarea'), {
+            target: {
+                value: 'Comentário 1',
+            }
+        });
+        fireEvent.click(screen.getByTestId('elemento-btn'));
+            
+        fireEvent.change(screen.getByTestId('elemento-textarea'), {
+            target: {
+                value: 'Comentário 2',
+            }
+        });
+        fireEvent.click(screen.getByTestId('elemento-btn'));
+
+        expect(screen.getAllByTestId('qtd-comentario')).toHaveLength(2);
     });
 });
